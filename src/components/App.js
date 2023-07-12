17be6128fd4d7d3e644e1e9bba2897eeb552ceda
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-13 00:37:28 +0530

    submitting code-Thu Jul 13 2023 00:37:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+const Parent = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-export default App
+  const handleButtonClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <h2>Child Component 1</h2>
+      <Child buttonText="Option 1" handleClick={handleButtonClick} />
+      <h2>Child Component 2</h2>
+      <Child buttonText="Option 2" handleClick={handleButtonClick} />
+      <p>Selected Option: {selectedOption}</p>
+    </div>
+  );
+};
+
+const Child = ({ buttonText, handleClick }) => {
+  return (
+    <div className="child">
+      <button onClick={() => handleClick(buttonText)}>{buttonText}</button>
+    </div>
+  );
+};
+
+export default Parent;
